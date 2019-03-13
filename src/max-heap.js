bdd255bf2280f47b2bea4385e84e155d48363735
Katsiaryna Kananchuk <katekononchyk@gmail.com>
2019-03-13 16:53:08 +0300

    feat: add clear and puch functions

--- src/max-heap.js
+++ src/max-heap.js
@@ -4,18 +4,32 @@ class MaxHeap {
 	constructor() {
 		this.parentNodes = [];
 		this.root = null;
+
+		this.size = 0;
 	}
 
 	push(data, priority) {
-		
+		this.size++;
+
+		const newNode = new Node(data, priority);
+
+		this.insertNode(newNode);
+		this.shiftNodeUp(newNode);
 	}
 
 	pop() {
-		
+		if (!this.size ===0 ){
+			this.size--;
+
+		}
 	}
 
 	detachRoot() {
-		
+		let detachedRoot = this.root;
+		this.root = null;
+
+		return detachedRoot;
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
@@ -23,7 +37,7 @@ class MaxHeap {
 	}
 
 	size() {
-		return this.data.length;
+		return this.size;
 	}
 
 	isEmpty() {
@@ -32,10 +46,17 @@ class MaxHeap {
 
 	clear() {
 		
+		this.parentNodes = [];
+		this.root = null;
+
+		this.size = 0;
 	}
 
 	insertNode(node) {
-		
+		if (!this.root) {
+			this.root = node;
+			this.parentNodes.push(node);
+		}
 	}
 
 	shiftNodeUp(node) {
